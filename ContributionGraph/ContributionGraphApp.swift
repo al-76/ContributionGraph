539825105484e305d25231e71186b2e407718efb
Vyacheslav Konopkin <robotal76@gmail.com>
2022-06-20 18:12:05 +0300

    Initial Commit

---
+++ ContributionGraph/ContributionGraphApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContributionGraphApp.swift
+//  ContributionGraph
+//
+//  Created by Vyacheslav Konopkin on 20.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContributionGraphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
