89ed9cd49cb1b3dbd718849fa55da098936f26b0
Vyacheslav Konopkin <robotal76@gmail.com>
2022-09-06 18:52:36 +0300

    Add SetContributionSettingsUseCaseTests

---
+++ ContributionGraphTests/Domain/UseCase/ContributionSettings/SetContributionSettingsUseCaseTests.swift
@@ -0,0 +1,31 @@
+//
+//  SetContributionSettingsUseCaseTests.swift
+//  ContributionGraphTests
+//
+//  Created by Vyacheslav Konopkin on 06.09.2022.
+//
+
+import XCTest
+
+@testable import ContributionGraph
+
+class SetContributionSettingsUseCaseTests: XCTestCase {
+    private var useCase: DefaultSetContributionSettingsUseCase!
+
+    override func setUp() {
+        super.setUp()
+
+        useCase = DefaultSetContributionSettingsUseCase()
+    }
+
+    func testExecute() throws {
+        // Arrange
+        let testData = ContributionSettings(weekCount: 15)
+
+        // Act
+        let result = try awaitPublisher(useCase(testData))
+
+        // Assert
+        XCTAssertEqual(result, testData)
+    }
+}
