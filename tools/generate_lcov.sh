bc6692f12328dfcb91f21d0c498675a78555c8de
Vyacheslav Konopkin <robotal76@gmail.com>
2022-09-09 23:42:07 +0300

    Update generate_lcov.sh

--- tools/generate_lcov.sh
+++ tools/generate_lcov.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-XCODE_DERIVED_DATA=~/Library/Developer/Xcode/DerivedData/ContributionGraph-*/Build
 APP_NAME=ContributionGraph
+XCODE_DERIVED_DATA=~/Library/Developer/Xcode/DerivedData/$APP_NAME-*/Build
+PROFDATA=`readlink -f $XCODE_DERIVED_DATA/ProfileData/*/Coverage.profdata`
+COVERAGE=`readlink -f $XCODE_DERIVED_DATA/Products/Debug-iphonesimulator/$APP_NAME.app/$APP_NAME`
 
-PROFDATA=`ls $XCODE_DERIVED_DATA/ProfileData/*/Coverage.profdata`
-COVERAGE=`ls $XCODE_DERIVED_DATA/Products/Debug-iphonesimulator/$APP_NAME.app/$APP_NAME`
 xcrun llvm-cov export -format=lcov -instr-profile=$PROFDATA $COVERAGE > coverage.lcov
